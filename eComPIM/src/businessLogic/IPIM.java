d7c20613057cbfe1be8e4a5186f0eb2ff8798844
Hoodedgull <victor.s@live.dk>
2017-04-24 14:15:39 +0200

    Rename

--- eComPIM/src/businessLogic/IPIM.java
+++ eComPIM/src/businessLogic/IPIM.java
@@ -1,11 +1,15 @@
 package businessLogic;
 
+import ecompim.Product.Product;
+
+import java.util.HashMap;
+
 /**
  * Created by victo on 2017-04-24.
  */
-public interface PIMInterface extends ProductFetcherInterface{
+public interface IPIM extends IProductFetcher {
 
-    public Product[] fetchProducts();
+    public HashMap<Integer,Product> fetchProducts();
     public void changeProductAttribute(int productID, String attribute, String newValue);
     public void saveChanges();
 
--- eComPIM/src/businessLogic/IProductFetcher.java
+++ eComPIM/src/businessLogic/IProductFetcher.java
@@ -1,9 +1,11 @@
 package businessLogic;
 
+import ecompim.Product.*;
+
 /**
  * Created by victo on 2017-04-24.
  */
-public interface ProductFetcherInterface {
+public interface IProductFetcher {
 
     public DetailedProduct fetchProduct(int productID);
     public Product[] searchProducts(String searchCriteria);
