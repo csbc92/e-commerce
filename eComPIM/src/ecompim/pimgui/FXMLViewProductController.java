7fec9d0a030d03dbb0405e75b91092de02392255
Hoodedgull <victor.s@live.dk>
2017-04-25 14:48:44 +0200

    ViewProductGUI WIP

---
+++ eComPIM/src/ecompim/pimgui/FXMLViewProductController.java
@@ -0,0 +1,29 @@
+package ecompim.pimgui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Observable;
+import java.util.ResourceBundle;
+
+import ecompim.Product.Product;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import ecompim.businessLogic.PIMManager;
+import ecompim.businessLogic.IPIM;
+
+/**
+ * Created by victo on 2017-04-25.
+ */
+public class FXMLViewProductController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
