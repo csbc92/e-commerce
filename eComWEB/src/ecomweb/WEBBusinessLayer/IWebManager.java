0328383981539938f5066e033fa8862075fe968d
Daniel Johansen <dajoh16@student.sdu.dk>
2017-05-05 08:41:35 +0200

    Jar fil for PIM
    Der er blevet genereret en Jar fil for PIM, som er blevet lagt som et library i WEB

--- eComWEB/src/ecomweb/WEBBusinessLayer/IWebManager.java
+++ eComWEB/src/ecomweb/WEBBusinessLayer/IWebManager.java
@@ -1,9 +1,11 @@
 package ecomweb.WEBBusinessLayer;
 
+import ecompim.Product.Product;
+
 /**
  * Created by Vedsted on 24-04-2017.
  */
 public interface IWebManager {
 
-    //public Product getProduct(int productID);
+    public Product getProduct(int productID);
 }
