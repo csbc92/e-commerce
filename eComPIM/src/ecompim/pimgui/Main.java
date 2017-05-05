4968788d264e934b9b2434ccb5712fd9d35968be
Hoodedgull <victor.s@live.dk>
2017-05-05 08:40:50 +0200

    pimgui -> PIMGUI

--- eComPIM/src/ecompim/pimgui/Main.java
+++ eComPIM/src/ecompim/pimgui/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ecompim.pimgui;
+package ecompim.PIMGUI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
