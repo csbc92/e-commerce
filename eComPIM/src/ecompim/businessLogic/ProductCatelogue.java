e626af6d26457a2a2b192a310046cc36ae9c6297
JV <jv@coresoft.dk>
2017-05-12 16:56:38 +0200

    ProductCatelogue

---
+++ eComPIM/src/ecompim/businessLogic/ProductCatelogue.java
@@ -0,0 +1,14 @@
+package ecompim.businessLogic;
+
+import ecompim.PIMPersistence.PIMPersistenceFacade;
+
+/**
+ * Created by JV on 12-05-2017.
+ */
+public class ProductCatelogue {
+
+    private PIMPersistenceFacade persistance;
+
+    public ProductCatelogue() {
+    }
+}
