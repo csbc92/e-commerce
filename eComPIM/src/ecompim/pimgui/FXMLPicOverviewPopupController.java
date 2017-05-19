b9859e813b0afcab8495eefb46ea99566393d873
Hoodedgull <victor.s@live.dk>
2017-05-19 14:38:13 +0200

    Popup viser nu billeder

--- eComPIM/src/ecompim/pimgui/FXMLPicOverviewPopupController.java
+++ eComPIM/src/ecompim/pimgui/FXMLPicOverviewPopupController.java
@@ -5,6 +5,9 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,16 +19,19 @@ import java.util.ResourceBundle;
  */
 public class FXMLPicOverviewPopupController implements Initializable {
     @FXML
-    private ListView lvPicOverview;
+    private ListView<ImageView> lvPicOverview;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        ArrayList<String> picList = new ArrayList<>();
-        ObservableList<String> oList = FXCollections.observableArrayList();
+        ArrayList<ImageView> picList = new ArrayList<>();
+        ObservableList<ImageView> oList = FXCollections.observableArrayList();
 
         for (int i = 1; i < 10; i++) {
             try {
-                picList.add(FXMLDocumentController.manager.fetchMedia(i).toURI().toString());
+                ImageView tmp = new ImageView(new Image(FXMLDocumentController.manager.fetchMedia(i).toURI().toString()));
+                tmp.setFitHeight(100);
+                tmp.setPreserveRatio(true);
+                picList.add(tmp);
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (ClassNotFoundException e) {
