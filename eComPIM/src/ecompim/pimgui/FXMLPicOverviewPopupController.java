aba6da7a5257718faec81e281f5f37ea0a678ece
Hoodedgull <victor.s@live.dk>
2017-05-19 14:28:27 +0200

    Default picture

--- eComPIM/src/ecompim/pimgui/FXMLPicOverviewPopupController.java
+++ eComPIM/src/ecompim/pimgui/FXMLPicOverviewPopupController.java
@@ -1,10 +1,14 @@
 package ecompim.pimgui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 /**
@@ -16,6 +20,20 @@ public class FXMLPicOverviewPopupController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        ArrayList<String> picList = new ArrayList<>();
+        ObservableList<String> oList = FXCollections.observableArrayList();
 
+        for (int i = 1; i < 10; i++) {
+            try {
+                picList.add(FXMLDocumentController.manager.fetchMedia(i).toURI().toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        oList.setAll(picList);
+        lvPicOverview.setItems(oList);
     }
 }
