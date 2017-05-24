205534929f099eb829f8da9da99fbdc893c78a60
Mabar16 <stone1996@live.dk>
2017-05-24 13:51:32 +0200

    JDocs for PIM.
    Alt er JDoc'd udover Networking

--- eComPIM/src/ecompim/PIMPersistence/IPIMPersistenceFacade.java
+++ eComPIM/src/ecompim/PIMPersistence/IPIMPersistenceFacade.java
@@ -10,19 +10,54 @@ import java.util.Map;
  */
 public interface IPIMPersistenceFacade{
 
+        /**
+         * Gets a map of simple products.
+         * @return HashMap containging instances of Product. 
+         */
         public HashMap<Integer, Product> fetchProductOverview() ;
 
+        /**
+         * Collects a DetailedProduct based on the specified ID
+         * @param productID ID of the product to be collected.
+         * @return The chosen product as instance of DetailedProduct.
+         */
         public DetailedProduct fetchProduct(int productID);
 
+        /**
+         * Stores the given map of products.
+         * @param products Map of products to be stores.
+         */
         public void storeProducts(Map<Integer, DetailedProduct> products) ;
 
+        /**
+         * Saves the specified product
+         * @param product Product to be saved.
+         */
         public void saveProduct(DetailedProduct product);
 
+        /**
+         * Search list of products for a String Value
+         * @param value String to be searched for.
+         * @return Map of products matching the search String
+         */
         public HashMap<Integer, Product> searchProducts(String value);
 
+        /**
+         * Fetches a map of simple products from a specified category.
+         * @param category The category to be searched
+         * @return a HashMap of the found products.
+         */
         HashMap<Integer, Product> fetchCategoryOverview(Category category);
 
+        /**
+         * returns the root of the category tree
+         * @return instance of category that is the root.
+         */
         public Category getRootCategory();
 
+        /**
+         * saves a category as the root.
+         * @param rootCategory
+         */
         public void saveRootCategory(Category rootCategory);
 }
