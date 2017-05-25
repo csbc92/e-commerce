fb2a003526df559591f8d46cd8192324af7b3643
Daniel Johansen <dajoh16@student.sdu.dk>
2017-05-25 10:52:41 +0200

    Frames til netværk
    En frame der indeholder et medie eller produkt samt en status kode

---
+++ ProductLib/ProductLib/src/Networking/DisplayableFrame.java
@@ -0,0 +1,24 @@
+package Networking;
+
+import Product.IDisplayable;
+
+/**
+ * Created by danie on 25-05-2017.
+ */
+public class DisplayableFrame {
+    private StatusCode statusCode;
+    private IDisplayable media;
+
+    public DisplayableFrame(StatusCode statusCode, IDisplayable media) {
+        this.statusCode = statusCode;
+        this.media = media;
+    }
+
+    public StatusCode getStatusCode() {
+        return statusCode;
+    }
+
+    public IDisplayable getMedia() {
+        return media;
+    }
+}
