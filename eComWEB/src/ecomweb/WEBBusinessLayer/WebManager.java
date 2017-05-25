301ba58305aeb690dc29cd26f418107a289731c2
Kaptajnen <csbc92@hotmail.com>
2017-05-26 00:50:38 +0200

    File one line fix in WebManager

--- eComWEB/src/ecomweb/WEBBusinessLayer/WebManager.java
+++ eComWEB/src/ecomweb/WEBBusinessLayer/WebManager.java
@@ -1,1 +1,42 @@
-package ecomweb.WEBBusinessLayer;import Networking.DetailedProductFrame;import Product.DetailedProduct;import ecomweb.network.ClientHandler;import network.FTPTool;import java.io.*;/** * Created by Vedsted on 24-04-2017. */public class WebManager implements IWebManager {    private ClientHandler client;    private FTPTool ftpTool;    public WebManager() {        client = new ClientHandler();        ftpTool = new FTPTool(FTPTool.username, FTPTool.password);    }    @Override    public DetailedProduct getProduct(int productID) {        DetailedProduct detailedProduct = client.getProduct(productID);        return detailedProduct;    }    @Override    public String getMediaPath(String mediaPath) {        System.out.println("manager path: " + mediaPath);        return ftpTool.retrieveFile(mediaPath.trim());    }}
+package ecomweb.WEBBusinessLayer;
+
+
+import Networking.DetailedProductFrame;
+import Product.DetailedProduct;
+import ecomweb.network.ClientHandler;
+import network.FTPTool;
+
+import java.io.*;
+
+
+/**
+ * Created by Vedsted on 24-04-2017.
+ */
+
+public class WebManager implements IWebManager {
+
+    private ClientHandler client;
+    private FTPTool ftpTool;
+
+    public WebManager() {
+
+        client = new ClientHandler();
+        ftpTool = new FTPTool(FTPTool.username, FTPTool.password);
+    }
+
+
+    @Override
+    public DetailedProduct getProduct(int productID) {
+        DetailedProduct detailedProduct = client.getProduct(productID);
+        return detailedProduct;
+    }
+
+    @Override
+    public String getMediaPath(String mediaPath) {
+        System.out.println("manager path: " + mediaPath);
+        return ftpTool.retrieveFile(mediaPath.trim());
+    }
+
+
+}
+
