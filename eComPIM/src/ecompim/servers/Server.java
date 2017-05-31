d5ba06c01cf47c719cfb295fe454f1d24fd94666
JV <jv@coresoft.dk>
2017-05-31 14:34:43 +0200

    Header changes

--- eComPIM/src/ecompim/servers/Server.java
+++ eComPIM/src/ecompim/servers/Server.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ecompim.servers;
 
 import Product.DetailedProduct;
@@ -15,10 +11,6 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketException;
 
-/**
- *
- * @author JV
- */
 public class Server implements Runnable {
 
     private ServerTool serverTool;
