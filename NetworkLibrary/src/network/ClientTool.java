ad97b0c73823614a6325e8370db1af430e35d5e4
Jonas Vedsted Sørensen <jonso16@student.sdu.dk>
2017-06-01 13:20:42 +0200

    changes in product and network

--- NetworkLibrary/src/network/ClientTool.java
+++ NetworkLibrary/src/network/ClientTool.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 
 public class ClientTool {
 
-    public Socket clientSocket;
+    private Socket clientSocket;
     private ObjectOutputStream objectToServer;
     private DataInputStream stringFromServer;
     private ObjectInputStream objFromServer;
