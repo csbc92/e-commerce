c02688d2448cea4bb11191ebbde0fc328cfb577e
Daniel Johansen <dajoh16@student.sdu.dk>
2017-06-01 11:37:24 +0200

    Title på webshop

--- eComWEB/src/ecomweb/webgui/FXMLDocumentController.java
+++ eComWEB/src/ecomweb/webgui/FXMLDocumentController.java
@@ -68,6 +68,7 @@ public class FXMLDocumentController implements Initializable {
             lPrice.setText("Pris: " + product.getSalePrice() + " dkk");
             lProductName.setText(product.getName());
             lProductID.setText("ID: " + product.getProductID());
+            lLongDescriptionSection.setText(product.getName());
             taLongDescription.setText(product.getLongDescription());
             populateTechnicalDetails(tblViewTechDetails, product);
 
